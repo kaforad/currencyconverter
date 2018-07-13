54c97b66bc465460b262e3bf14e47b79556afdf1
kaforad <kaforad@gmail.com>
2018-07-13 21:43:48 +0100

    refactor currency conversion function/module

--- kaforad-converter.js
+++ kaforad-converter.js
@@ -26,8 +26,7 @@ bindData(results,bindToObject){
  				let currencyOption = document.createElement('option');
  				currencyOption.setAttribute('value', results[key].id);
  				currencyOption.setAttribute('label', results[key].currencyName); 
- 				currencyOption.setAttribute('text',  results[key].currencyName);
- 				//I had to use .innerHTML to handle label attribute display for mozilla
+ 				currencyOption.setAttribute('text',  results[key].currencyName); 			
  				currencyOption.innerHTML = results[key].currencyName;
   				selectCurrency.appendChild(currencyOption);
   				
@@ -36,77 +35,69 @@ bindData(results,bindToObject){
 		return;
 }
 
+rateCalculation(amount,rate){
+	//currency conversion
+	let total =0;	
+	total =  Math.round((amount*rate) * 100) / 100;
+	return total;
+	
+}
+
 convertCurrency(){
-	 	// this variables stores returned values from the API
-	   	let fromVal =0;
-	   	let toVal=0;
-	 	//Get conversion data(amount to convert, currency to convert to and currencty to convdrt from)
-		let button = document.getElementById("btnConvert");
-		let drpFrm='';
-		let drpTo='';
+
+	var drpFrom=document.getElementById("convertFrom");
+	var drpTo=document.getElementById("convertTo");
+
+	var button = document.getElementById("btnConvert");
+
 		button.addEventListener("click",function(e){
+			//this.amountToConvert = document.getElementById("amount").value;
+			let amount = document.getElementById("amount").value;
+			this.fromCurrency=drpFrom.value;
+    		this.toCurrency=drpTo.value;
+    		let fromSelected=drpFrom.options[drpFrom.selectedIndex].text;
+			let toSelected=drpTo.options[drpTo.selectedIndex].text;
+			
+    		// creating the API query string as seen in documentation
+    		let queryString = this.fromCurrency+ '_' + this.toCurrency +','+this.toCurrency+ '_' + this.fromCurrency;
 
-			let amount = this.amountToConvert = document.getElementById("amount").value;
-  
-    			this.fromCurrency=document.getElementById("convertFrom").value;
-    			this.toCurrency=document.getElementById("convertTo").value;
-    			let selectedFrom= document.getElementById("convertFrom");
-    			let selectedTo= document.getElementById("convertTo");
-    			 drpFrm = selectedFrom.options[selectedFrom.selectedIndex].text;
-    			drpTo = selectedTo.options[selectedTo.selectedIndex].text;
-    			 
-    			
+    		//call currency converter API - Multiple queries, and non-compact sample
+			fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${queryString}`, {method: 'get'})						
+			.then((conversionRate) => conversionRate.json()) 				
+			.then(function(rateValue) {	
+				console.log(rateValue.results);
+				let currencyIndex =0;
+				let currencyKey = [];
+				Object.keys(rateValue.results).forEach(function(key){			
+					
+					    currencyKey[currencyIndex] = key;						
+					    currencyIndex += 1;
+					});
+				
+					//get conversion rate from and to
+					console.log('rate value results');
+					console.log(rateValue.results);
+				   	let currencyCode=rateValue.results[currencyKey[0]].fr;
+					let currencyValue =rateValue.results[currencyKey[0]].val;
+					let reverseCode=rateValue.results[currencyKey[1]].fr;
+					let reverseValue=rateValue.results[currencyKey[1]].val;
 
-    			// creating the API query string as seen in documentation
-    			let query = this.fromCurrency+ '_' + this.toCurrency +','+this.toCurrency+ '_' + this.fromCurrency;
-    			
-				//call currency converter API - Multiple queries, and non-compact sample
-				fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}`, {method: 'get'})						
-				.then((conversionRate) => conversionRate.json()) 				
-				.then(function(rateValue) {				
-					// working around, object refrencing with index rather than with key
-					// I should try refactor this later considering for loop but for now let me get the project done
-					let currencykey=[];
-					let i=	0;					
-					Object.keys(rateValue.results).forEach(function(key){			
+					//create conversion rate string display
+					let buildConversionString = `1 ${currencyCode} = ${reverseCode} ${reverseValue}`;
+					let buildReverseString = `1 ${reverseCode} = ${currencyCode} ${currencyValue}`;
+					let total = cConverter.rateCalculation(amount ,currencyValue);
+					document.getElementById("countryCurrency").innerHTML = `${fromSelected} &nbsp; to &nbsp; ${toSelected}`;					
+					document.getElementById("currencyConversion").innerHTML = buildConversionString +'&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; '+ buildReverseString;						
+					document.getElementById("conversionRate").innerHTML = `<b> ${amount} ${currencyCode} = ${total} ${reverseCode}</b>`;
+			})	.catch(function(error) {
 					
-					   currencykey[i] =key;						
-						i =i+1;
-					})
-					//since only two values will be returned at any point in time with the free API version
-					// I am safe to reference index 0 and 1 , index 0 for where I am converting from
-					// index 1 for reverse conversion
-					let fromVal=rateValue.results[currencykey[0]].fr;
-						let rateVal =rateValue.results[currencykey[0]].val;
-						let reverseCurrency=rateValue.results[currencykey[1]].fr;
-						let reverseVal=rateValue.results[currencykey[1]].val;
-
-					//calculate converstion rate by multiplying user value with API value at 1
-					   let   finalValue =  Math.round((amount*rateVal) * 100) / 100;
-					  let   finalReverse = Math.round((amount*reverseVal) * 100) / 100;
-					
-					   //finalValue = finalValue.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-					   //finalReverse = finalReverse.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-					   
-					  
-					
-					    let buildConversionString = `1 ${fromVal} = ${reverseCurrency} ${rateVal}`;
-					    let buildReverseString = `1 ${reverseCurrency} = ${fromVal} ${reverseVal}`;
-					    document.getElementById("conversionRate").innerHTML = `<b> ${amount} ${fromVal} = ${finalValue} ${reverseCurrency}</b>`;
-						document.getElementById("conversionCurrency").innerHTML = buildConversionString +'&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; '+buildReverseString;						
-						document.getElementById("countryCurrency").innerHTML = `${drpFrm} &nbsp; to &nbsp; ${drpTo}`;
-					
-					})
-				.catch(function(error) {
-					// Error issues to be handled here
-				  throw Error(error);
+				  console.log(error);
 				    document.getElementById("errorDisplay").innerHTML = "Conversion rate can not be gotten at this time. <p>Confirm you are online";
 				 }); 
-    	
-						
-		},false);
 
-	}
+
+		},false);
+}
 
 
 
@@ -120,8 +111,8 @@ let cConverter = new CurrencyConverter();
 (function(){
 	fetch('https://free.currencyconverterapi.com/api/v5/currencies', {method: 'get'})
 			
-				.then((currencies) => currencies.json()) //get API currencies into json
-				 // Get all currencies from API and manipulate
+				.then((currencies) => currencies.json()) 
+				 // Manipulating Currency Data from FREE API
 				.then(function(allCurrencies) {
 						
 		             //call funciton - binding currency to drop down
